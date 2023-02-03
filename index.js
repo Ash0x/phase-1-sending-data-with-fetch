af097bb3829020744f9975a9663de6db21566d31
Alexander <alex.galev@yahoo.com>
2023-02-03 14:19:09 -0700

    passing tests

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Add your code here
+const submitData = (name, email) => {
+
+	return fetch('http://localhost:3000/users', {
+		method: 'POST',
+		headers: {
+			'Content-Type': 'application/json',
+			'Accept': 'application/json'
+		},
+		body: JSON.stringify({ name: name, email: email })
+	})
+		.then((res) => {
+			if (res.ok) {
+				return res.json()
+			}
+			throw new Error(res.status)
+		})
+		.then((data) => {return data})
+		.catch((error) => {
+			document.body.append(error)
+		})
+}
